aec330f645f8ba8e85c6158074148592dabad46d
adiii7star <31256621+adiii7star@users.noreply.github.com>
2017-08-24 10:30:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-   res.send('Article One reuested and will be served'); 
+   res.send('Article One requested and will be served'); 
 });
 
 app.get('/article-two',function(req, res){

1c04892a0146d469cf9afaaeccd2ce9f49cd669e
adiii7star <31256621+adiii7star@users.noreply.github.com>
2017-08-25 12:36:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
-console.log('Loaded fast!');
-
-//Now will add some changes
-var element=document.getElementById("main-id");
-element.InnerHtml="Sophistication";
-
-//Now will try to move some elements
-
-var img=document.getElementById('madi');
-img.onclick=function(){
-    img.style.marginleft='100px';
+//Now we will use the button to add the functionality
+var button=getElementById('counter');
+var counter=0;
+button.onclick=function(){
+  //Make a request to the counter endpoint
+  
+  //Capture the enpoint in the variabe
+  
+  //Render the variable in the correct span
+  counter=counter+1;
+  var span=getElementById('count');
+  span.innerHtml=counter.toString(); 
 };

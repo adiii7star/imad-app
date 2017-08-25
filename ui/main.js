bca2bc586b17c3f52e9a1370172d93894ac850b9
adiii7star <31256621+adiii7star@users.noreply.github.com>
2017-08-25 11:59:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //Now will add some changes
 var element=document.getElementById("main-id");
 element.InnerHtml="Sophistication";
+
+//Now will try to move some elements
+
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
